6e42897b1b0f33ca7070aa8fb9fc9599c1bdf4b5
LIU Zhao <liuzhaomax@163.com>
2022-09-26 16:46:28 +1000

    增加数据

--- test_data/demo.go
+++ test_data/demo.go
@@ -10,12 +10,12 @@ func CreateDemoData(db *gorm.DB) {
 	var data model.Demo
 	result := db.First(&data)
 	if result.RowsAffected == 0 {
-		for i := 0; i < 6; i++ {
+		for i := 0; i < 18; i++ {
 			var demo model.Demo
 			demo.Title = fmt.Sprintf("小刘鸭%d", i)
 			demo.Desc = "我是一只丑小鸭"
 			demo.Content = "for i, abc = range abcs{}"
-			demo.View = 5
+			demo.View = uint32(i)
 			demo.Preview = "http://10.192.0.5:9202/static/demo/duck_liu/02.jpg"
 			demo.Ref = "https://www.baidu.com"
 			db.Create(&demo)
